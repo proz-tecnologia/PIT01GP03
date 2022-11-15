fcd8a74aedda47f379791dcebe481d965c4532b1
charleston <charles1235223@gmail.com>
2022-11-15 13:38:07 -0400

    add invisible password and delete old files

---
+++ lib/view/components/formfield_register_password.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_flutter/view/components/formfield_register.dart';
+
+class FormRegisterPassword extends StatefulWidget {
+  final String textRegister;
+
+  const FormRegisterPassword({
+    Key? key,
+    required this.textRegister,
+  }) : super(key: key);
+
+  @override
+  State<FormRegisterPassword> createState() => _FormRegisterPasswordState();
+}
+
+class _FormRegisterPasswordState extends State<FormRegisterPassword> {
+  bool _isHidden = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return FormRegister(
+      textRegister: widget.textRegister,
+      obscureText: _isHidden,
+      suffixIcon: InkWell(
+          borderRadius: BorderRadius.circular(23.0),
+          onTap: () {
+            setState(() {
+              _isHidden = !_isHidden;
+            });
+          },
+          child: Icon( _isHidden ? Icons.visibility : Icons.visibility_off )),
+    );
+  }
+}
