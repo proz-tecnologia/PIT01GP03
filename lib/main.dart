6197ddf00ab6e50659f9d90606089da92bb20d8d
Paulo V G H Amorim <paulovgha@gmail.com>
2022-12-13 17:27:48 -0300

    set up Firebase Authentication plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:porkinio/my_app.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -8,6 +9,9 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+
   runApp(
     const MyApp(),
   );
