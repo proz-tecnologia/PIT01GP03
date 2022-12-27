f9a7b221e3bdda9629aa562800ad74fec16c7bff
bia_lima <beatriz.pslima@gmail.com>
2022-12-27 13:23:26 -0300

    re-writing error messages

--- lib/app/services/firebase_auth_service.dart
+++ lib/app/services/firebase_auth_service.dart
@@ -23,19 +23,19 @@ class FirebaseAuthService implements AuthService {
     } on FirebaseAuthException catch (e) {
       switch (e.message) {
     case 'The email address is already in use by another account.':
-      throw 'Email já foi cadastrado';
+      throw 'Este E-mail já foi cadastrado';
 
     case 'The password is invalid or the user does not have a password.':
-      throw 'Sua senha está errada';
+      throw 'Sua senha está errada. Tente novamente';
 
     case 'There is no user record corresponding to this identifier. The user may have been deleted.':
-      throw 'Usuario não foi criado';
+      throw 'Usuário sem cadastro. Insira um login com informações previamente cadastradas';
 
     case 'Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.':
       throw 'Seu acesso está temporariamente indisponível devido às diversas tentativas de login. Resete a senha ou tente novamente mais tarde';
 
     default:
-      throw 'Um erro indefinido ocorreu.';
+      throw 'Ops, um erro ocorreu. Verifique sua conexão com a internet e/ou tente novamente';
   }
       
     } catch (e) {
