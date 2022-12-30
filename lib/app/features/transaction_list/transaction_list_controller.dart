5acd192f12f8b1dc6b5003892927505dff231ee6
Paulo V G H Amorim <paulovgha@gmail.com>
2022-12-30 10:27:34 -0300

    implement firestore functionality to create and set new transactions

--- lib/app/features/transaction_list/transaction_list_controller.dart
+++ lib/app/features/transaction_list/transaction_list_controller.dart
@@ -1,17 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:porkinio/app/models/transaction_model.dart';
 import 'package:porkinio/app/services/mock_transaction.dart';
 
 class TransactionListController extends ChangeNotifier {
-
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
   final List<TransactionModel> transactionList = [...transactionsMock];
 
-  void addTransaction(TransactionModel transaction) {
-    final newTransaction =
-        transaction.copyWith(id: transaction.hashCode.toString());
-    transactionList.add(newTransaction);
+  Future addTransaction(TransactionModel transaction) async {
+    final newTransaction = firestore.collection('transactionDB').doc();
+    transaction.id = newTransaction.id;
+    final json = transaction.toJson();
+    await newTransaction.set(json);
     notifyListeners();
   }
+
+  // void addTransaction(TransactionModel transaction) {
+  //   final newTransaction =
+  //       transaction.copyWith(id: transaction.hashCode.toString());
+  //   transactionList.add(newTransaction);
+  //   notifyListeners();
+  // }
 
   void updateTransaction(TransactionModel transaction) {
     for (int i = 0; i < transactionList.length; i++) {
@@ -21,6 +30,15 @@ class TransactionListController extends ChangeNotifier {
     }
     notifyListeners();
   }
+
+  // void updateTransaction(TransactionModel transaction) {
+  //   for (int i = 0; i < transactionList.length; i++) {
+  //     if (transactionList[i].id == transaction.id) {
+  //       transactionList[i] = transaction;
+  //     }
+  //   }
+  //   notifyListeners();
+  // }
 
   void removeTransaction(TransactionModel transaction) {
     transactionList.remove(transaction);
--- lib/app/models/transaction_model.dart
+++ lib/app/models/transaction_model.dart
@@ -1,12 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class TransactionModel {
-  final String? id ;
+  String? id;
   final String title;
   final double ammount;
   final DateTime date;
   final bool category;
 
   TransactionModel({
-    this.id,
+    this.id = '',
     required this.title,
     required this.ammount,
     required this.date,
@@ -29,13 +34,38 @@ class TransactionModel {
     );
   }
 
-   Map<String, dynamic> toMap() {
+  Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'id': id,
       'title': title,
       'ammount': ammount,
-      'date': date,
+      'date': date.millisecondsSinceEpoch,
       'category': category,
     };
   }
+
+  factory TransactionModel.fromMap(Map<String, dynamic> map) {
+    return TransactionModel(
+      title: map['title'] as String,
+      ammount: map['ammount'] as double,
+      date: DateTime.fromMillisecondsSinceEpoch(map['date'] as int),
+      category: map['category'] as bool,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'ammount': ammount,
+        'date': date.millisecondsSinceEpoch,
+        'category': category,
+      };
+
+  static TransactionModel fromJson(Map<String, dynamic> json) =>
+      TransactionModel(
+        id: json['id'],
+        title: json['title'],
+        ammount: json['ammount'],
+        date: (json['date'] as Timestamp).toDate(),
+        category: json['category'],
+      );
 }
