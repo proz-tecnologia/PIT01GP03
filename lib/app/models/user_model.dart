487ee867f50329cd895b400420d05ba6dc32d18f
charleston <charles1235223@gmail.com>
2023-01-14 06:10:44 -0400

    test firebase auth service

--- lib/app/models/user_model.dart
+++ lib/app/models/user_model.dart
@@ -3,13 +3,13 @@ import 'dart:convert';
 class UserModel {
   final String? id;
   final String? name;
-  final String email;
+  final String? email;
   final String? password;
 
   UserModel({
     this.id,
     this.name,
-    required this.email,
+    this.email,
     this.password,
   });
 
---
+++ test/mock/mock_classes.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:porkinio/app/services/auth_service.dart';
+
+class MockFirebaseAuthService extends Mock implements AuthService {}
