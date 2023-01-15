9474d9d7413aaf935a839adb23b7dce85bfd252f
charleston <charles1235223@gmail.com>
2023-01-15 10:47:24 -0400

    add group test in CustomFormFieldTest

--- test/app/common/widgets/custom_form_field_test.dart
+++ test/app/common/widgets/custom_form_field_test.dart
@@ -3,11 +3,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:porkinio/app/common/widgets/custom_form_field.dart';
 
 void main() {
-  testWidgets('custom form field ...', (tester) async {
-     const tText = 'name';
-     await tester.pumpWidget( const MaterialApp(home: Material(child: CustomFormField(text: tText,))));
+  group('test in CustomFormField', (() {
 
-    final textFinder = find.text(tText);
-    expect(textFinder, findsOneWidget);
-  });
+
+    testWidgets('CustomFormField test text ', (tester) async {
+      const tname = 'name';
+      await tester.pumpWidget(const MaterialApp(
+          home: Material(
+              child: CustomFormField(
+        text: tname,
+      ))));
+
+      final textFinder = find.text(tname);
+      expect(textFinder, findsOneWidget);
+    });
+
+
+
+  }));
 }
