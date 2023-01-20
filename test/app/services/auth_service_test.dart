1749e4981869bae1afb09980482012ba26c08ea9
charleston <charles1235223@gmail.com>
2023-01-19 21:42:28 -0400

    import test auth_service

--- test/app/services/auth_service_test.dart
+++ test/app/services/auth_service_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
+// ignore: unused_import
+import 'package:porkinio/app/services/auth_service.dart';
 
 void main() {
   testWidgets('auth service ...', (tester) async {
-    // TODO: Implement test
   });
 }
+
